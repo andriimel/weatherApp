e60030ada19f3afb382f583071c0a7f4b3e4c8d2
Andrii <melnykandriy4@gmail.com>
2024-05-09 20:59:55 -0400

    added search view controller

---
+++ FreeWeatherApp/VC Elements/CustomCells/FWSettingsCustomCell.swift
@@ -0,0 +1,12 @@
+//
+//  FWSettingsCustomCell.swift
+//  FreeWeatherApp
+//
+//  Created by Andrii Melnyk on 5/9/24.
+//
+
+import UIKit
+
+class FWSettingsCustomCell: UITableCollectionViewCell {
+
+}
